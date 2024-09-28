1b16c2e08becc2e1c4040ca0dd5a57c0e4f82b58
Leynheker <LeybaxD@gmail.com>
2024-09-27 20:11:29 -0600

    Add files via upload

---
+++ Lmath.py
@@ -0,0 +1,17 @@
+def sumar(x, y):
+    print(x + y)
+def multiplicar(x, y):
+    print(x * y)
+def restar(x, y):
+    print(x - y)
+def raiz(x):
+    from math import sqrt
+    print(sqrt(x))
+def potencia(x, y):
+    print(x**y)
+def cdc(r): # vBeta!
+    print(r * 3,14)
+def adc(d): # vBeta!
+    print(d * 3,14)
+def p(x):
+    print(x)
---
+++ main.py
@@ -0,0 +1,6 @@
+import Lmath
+Lmath.sumar(1, 4)
+Lmath.multiplicar(2, 2)
+Lmath.restar(2, 1)
+Lmath.raiz(16)
+Lmath.potencia(3, 4)
